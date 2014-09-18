1ea2e05ce47c3059f749b5557a9d4f8e63e68f22
Sven Speckmaier <sps@ipunkt.biz>
2014-09-18 17:04:45 +0200

    Fix for nomaster view

--- src/views/nomaster.blade.php
+++ src/views/nomaster.blade.php
@@ -1,2 +1,17 @@
-@yield('content') 
-@stop
+<!doctype html>
+<html lang="en">
+@foreach($errors->all() as $message)
+    {{ $message }}
+@endforeach
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel PHP Framework</title>
+    <style>
+    </style>
+    @yield('head')
+</head>
+
+<body>
+@yield('content')
+</body>
+</html>
